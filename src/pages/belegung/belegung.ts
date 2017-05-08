97ea2bd18d6c712f032b8a4f02b52da366337453
samplb <a1401504@unet.univie.ac.at>
2017-05-08 23:43:51 +0200

    finish belegung. muss nur noch werte aus dem Storage lesen und ausgeben. QR code begonnen aber noch fehlerhaft

--- src/pages/belegung/belegung.ts
+++ src/pages/belegung/belegung.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Chart } from 'chart.js';
 
 /**
  * Generated class for the Belegung page.
  *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
+ * See https://www.joshmorony.com/adding-responsive-charts-graphs-to-ionic-2-applications/
  */
 @IonicPage()
 @Component({
@@ -14,12 +13,47 @@ import { Chart } from 'chart.js';
   templateUrl: 'belegung.html',
 })
 export class Belegung {
-
+    @ViewChild('doughnutCanvas') doughnutCanvas;
+    doughnutChart:any;
+    free_slots:number;
+    reserved_slots:number;
+    used_slots:number;
+    sumup:number;
+    
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+      
   }
-
+  createChart(){
+      this.free_slots=134;
+      this.reserved_slots=45;
+      this.used_slots=90;
+      this.sumup = this.free_slots + this.reserved_slots + this.used_slots;
+      this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+        type: 'doughnut',
+        data: {
+            labels: ["Frei", "Reserviert", "Belegt"],
+            datasets: [{
+                label:'# Belegung',
+                data: [this.free_slots, this.reserved_slots, this.used_slots],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.8)',
+                    'rgba(54, 162, 235, 0.8)',
+                    'rgba(255, 206, 86, 0.8)'
+                ],
+                hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                ]
+            }]
+        }
+    })
+  }
+   
   ionViewDidLoad() {
     console.log('ionViewDidLoad Belegung');
+    this.createChart();
+    
   }
 
 }
--- src/pages/qr-code/qr-code.ts
+++ src/pages/qr-code/qr-code.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
 
 /**
  * Generated class for the QrCode page.
  *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
+ * See https://www.thepolyglotdeveloper.com/2016/02/add-barcode-scanning-functionality-to-your-ionic-2-app/
  */
 @IonicPage()
 @Component({
@@ -14,7 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class QrCode {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private scanner:BarcodeScanner, public navCtrl: NavController, public navParams: NavParams) {
+     this.scan();
+      
+  }
+  
+  scan() {
+      this.scanner.scan().then((barcodeData) => {
+          console.log(barcodeData);
+      
+      })
   }
 
   ionViewDidLoad() {
